08e30af346964735df6b749c94877e81e75ac343
Hannah Tabea Roos <hannahtabea.roos@gmail.com>
2021-08-18 21:59:52 +0200

    Working tidymodels workflow
    Added more data, included minority sampling to deal with class imbalance, reduced amount of models for comparison and finalized the workflow,

--- tidymodels.R
+++ tidymodels.R
@@ -18,7 +18,9 @@ library(skimr)
 str(ibm_dat)
 skim(ibm_dat)
 
-
+# speed up processing if possible
+library(furrr)
+plan(multicore) 
 #-------------------------------------------------------------------------------
 # DATA WRANGLING
 #-------------------------------------------------------------------------------
@@ -49,82 +51,83 @@ turnover_rate
 
 # sample small to medium-sized data
 set.seed(123)
-ibm_126 <- sample_n(ibm_dat,126,replace = F)
+ibm_147 <- sample_n(ibm_dat,147,replace = F)
 
-
-#-------------------------------------------------------------------------------
-# VERY BASIC FEATURE PREPROCESSING
-#-------------------------------------------------------------------------------
 
 # look at data to find variables that probably do not have any predictive power
-colnames(ibm_126)
+colnames(ibm_147)
 
 
 # clean up data
-ibm_reduced <- ibm_126 %>%
+ibm_reduced <- ibm_147 %>%
                select(-c("DailyRate","EducationField", "EmployeeCount", "EmployeeNumber",
                            "MonthlyRate","StandardHours","TotalWorkingYears","StockOptionLevel",
                            "Gender", "Over18", "OverTime", "median_compensation"))
 
-
-# make sure that factor levels of attrition are correctly ordered
-levels(ibm_reduced$Attrition)
-ibm_reduced$Attrition <- factor(ibm_reduced$Attrition, levels = c("Yes", "No"))
-# double check
-levels(ibm_reduced$Attrition)
-
 #-------------------------------------------------------------------------------
-# MODELLING PART 
+# DATA PREPARATION
 #-------------------------------------------------------------------------------
 
+library(rsample)
 library(tidymodels)
-# for naive bayes
-library(discrim)
-library(klaR)
+
+#create 90/10 split
+ibm_split <- initial_split(ibm_reduced, prop = 9/10, strata = Attrition)
+
+# Create the training data
+train <- ibm_split %>%
+  training()
+
+test <- ibm_split %>%
+  testing()
+
+# Check class imbalance
+train %>%
+  group_by(Attrition) %>%
+  summarise(
+    n = n(),
+    perc = n/nrow(.)
+  )
 
 # create data folds for cross validation
-myFolds <- vfold_cv(ibm_reduced,
-                        v = 5,
-                        #repeats = 5,
-                        strata = Attrition)
+myFolds <- vfold_cv(train,
+                    v = 5,
+                    repeats = 5,
+                    strata = Attrition)
+# save resampling for caret to make fair comparisons later
+train_cv_caret <- rsample2caret(myFolds)
 
+#-------------------------------------------------------------------------------
+# FEATURE PREPROCESSING
+#-------------------------------------------------------------------------------
 
-# create a reusable recipe for feature preprocessing and model formula 
-base_recipe <- 
-  recipe(Attrition ~ ., data = ibm_reduced) %>% 
+library(themis)
+
+set.seed(9560)
+# create reusable recipe for all models
+ibm_rec_balanced <- train %>%
+  recipe(Attrition ~ .) %>%
   # normalize all numeric predictors
   step_normalize(all_numeric()) %>%
   # create dummy variables 
   step_dummy(all_nominal(), - all_outcomes()) %>%
   # remove zero variance predictors
-  step_nzv(all_predictors(), - all_outcomes()) 
-
-filter_recipe <- base_recipe %>%
+  step_nzv(all_predictors(), - all_outcomes()) %>%
   # remove highly correlated vars
-  step_corr(all_numeric(), threshold = 0.75) 
-  
+  step_corr(all_numeric(), threshold = 0.75) %>%
+  # deal with class imbalance
+  step_rose(Attrition)
+
 
 # create model-specific recipes
 log_spec <- 
   logistic_reg(penalty = tune(), mixture = tune()) %>% 
-  set_engine("glm") 
-
-glmnet_spec <- 
-  logistic_reg(penalty = tune(), mixture = tune()) %>% 
   set_engine("glmnet") 
-
-rf_spec <- 
-  rand_forest( trees = tune(), min_n = tune()) %>% 
-  set_engine("ranger") %>% 
-  set_mode("classification")
 
 xgb_spec <- 
   boost_tree( trees = tune(), tree_depth = tune()) %>%
   set_mode("classification")
 
-nb_spec <- 
-  discrim::naive_Bayes()%>%
-  set_mode("classification")
 
 
 # create a workflow SET
@@ -132,25 +135,18 @@ library(workflowsets)
 
 my_models <- 
   workflow_set(
-    preproc = list(simple = base_recipe, filter = filter_recipe),
-    models = list(glm = log_spec, glmnet = glmnet_spec, naive_bayes = nb_spec, 
-                  ranger = rf_spec, xgbTree = xgb_spec),
+    preproc = list(ibm_rec_balanced),
+    models = list(glmnet = log_spec,  xgbTree = xgb_spec),
     cross = TRUE
   )
 my_models
 
-# use no filter on glmnet or glm
-my_models <- 
-  my_models %>% 
-  anti_join(tibble(wflow_id = c("filter_glm", "filter_glmnet")), 
-            by = "wflow_id")
 
 # actual training
-model_res <- 
+model_race <- 
   my_models %>% 
-  workflow_map("tune_bayes", resamples = myFolds, initial = 5,
-               iter = 20,verbose = TRUE,
-               metrics = metric_set(bal_accuracy))
+  workflow_map("tune_grid", resamples = myFolds, initial = 5,verbose = TRUE,
+               metrics = metric_set(bal_accuracy, yardstick::precision, yardstick::recall, f_meas))
 
 
 
@@ -159,29 +155,44 @@ model_res <-
 # MODEL COMPARISON
 #-------------------------------------------------------------------------------
 
-#glm did not work
-try <- model_res %>% 
-       filter(wflow_id != "simple_glm")
+# show metrices for the models
+model_race %>% collect_metrics() %>%
+  group_by(wflow_id)
 
-# show metrices for all other models
-try %>% collect_metrics() %>%
-  group_by(wflow_id) %>%
-  summarise(performance = mean(mean))
+# show performance
+autoplot(model_race)
 
-#autoplot(model_res)
-autoplot(try)
+# select best workflow for the model that worked well
+best_results <- 
+  model_race %>% 
+  extract_workflow_set_result("recipe_glmnet") %>% 
+  select_best(metric = "f_meas")
+best_results
+
+# show final metrics for the training set
+glmnet_test_results <- model_race %>% 
+                          extract_workflow("recipe_glmnet") %>% 
+                          finalize_workflow(best_results) %>% 
+                          last_fit(split = ibm_split, metrics = metric_set(bal_accuracy, yardstick::precision, yardstick::recall, f_meas)) %>%
+                          collect_metrics()
+glmnet_test_results
+
 
 #-------------------------------------------------------------------------------
 # TO DO's
 #-------------------------------------------------------------------------------
 
-# find out why the classic glm won't work
-# improve model performance - why are the algorithms not able to detect positive cases? (precision fail)
-# set f1 to standard metric again
-# make the tuning process comparable to caret workflow
+# train test split and model performance on test data for caret
+# make tuning grids comparable to each other (caret's default tuning parameters)
+# create confusion matrix for tidymodels
+# make predictions with the winning model to find employees at risk to leave soon
+# track times for model tuning with profvis
 
-# finalize the last fit for the baseline and winning model
-# retrieve several performance metrics: confusion matrix + precision, recall, f1 and balanced accuracy for each of the models?
-# use predictions from winning model to find employees at risk to leave soon
-
+# done:
+# ----
+# more a bit more data: 147 instead of 126
+# included minority sampling to deal with class imbalance
+# reduced model comparison to two competing models only
+# included furrrs multicore processing
+# finalized workflow 
 
