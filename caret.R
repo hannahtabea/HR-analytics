0dede58a4741af70f1d3d6e4f6b96e8012dc4800
Hannah Tabea Roos <hannahtabea.roos@gmail.com>
2021-09-14 15:35:22 +0200

    Minor updates
    Use all data available, changed k and repeats for training, visualized ROC results, feature importance and added a bit of formatting.

--- caret.R
+++ caret.R
@@ -33,23 +33,19 @@ ibm_dat[ , `:=`(CompensationLevel = factor(fcase(
                                              levels = c("Below","Average","Above"))),
                                                                           by = .(JobLevel) ]
 
-
-
 # how much turnover in dataset?
 turnover_rate <- ibm_dat[, list(count = .N, rate = (.N/nrow(ibm_dat))), by = Attrition]
 turnover_rate
 
-# sample small to medium-sized data
-set.seed(123)
-ibm_147 <- ibm_dat[sample(.N, 147)]
-
-
+# # sample small to medium-sized data
+# set.seed(123)
+# ibm_147 <- ibm_dat[sample(.N, 147)]
+ 
 # look at data to find variables that probably do not have any predictive power
 colnames(ibm_147)
 
-
 # clean up data
-ibm_reduced <- ibm_147[,-c("DailyRate","EducationField", "EmployeeCount", "EmployeeNumber",
+ibm_reduced <- ibm_dat[,-c("DailyRate","EducationField", "EmployeeCount", 
                            "MonthlyRate","StandardHours","TotalWorkingYears","StockOptionLevel",
                            "Gender", "Over18", "OverTime", "median_compensation")]
 
@@ -73,15 +69,14 @@ trainIndex <- createDataPartition(ibm_reduced$Attrition, p = .7,
 train <- ibm_reduced[ trainIndex,]
 test  <- ibm_reduced[-trainIndex,]
 
-
 ## deal with class imbalance - upsampling
 library(ROSE)
+library(dplyr)
 set.seed(9560)
 train <- ROSE(Attrition ~ ., data  = train)$data %>% 
   mutate(Attrition = factor(Attrition, levels = c("Yes", "No"))) # ROSE has reversed factor levels, therefore order them again...
 # check if it worked
 table(train$Attrition)
-
 
 f1 <- function (data, lev = NULL, model = NULL) {
   precision <- posPredValue(data$pred, data$obs, positive = "Yes")
@@ -92,23 +87,22 @@ f1 <- function (data, lev = NULL, model = NULL) {
 }
 
 # create k folds - not needed if borrowed from tidymodels 
-myFolds <- createFolds(train$Attrition, k = 5)
+myFolds <- createFolds(train$Attrition, k = 10)
 
-# Create reusable trainControl object: myControl
+# Create reusable trainControl object
 myControl <- trainControl(
   method = "repeatedcv", 
-  repeats = 5, 
+  repeats = 3, 
   summaryFunction = f1,
-  classProbs = TRUE, # IMPORTANT!
+  classProbs = TRUE, 
   verboseIter = TRUE,
   savePredictions = "final",
   returnResamp = "final",
   preProcOptions = list(cutoff = 0.75),
-  index = myFolds,
+  index = myFolds
   # index = train_cv_caret$index, # use when split is borrowed from tidymodels
   # indexOut = train_cv_caret$indexOut # see above
 )
-
 
 # Create reusable train function
 methods <- c("glmnet", "xgbTree")
@@ -126,7 +120,6 @@ train_model <- function(x) {
   return(assign(paste0("model_", x),model, envir = .GlobalEnv))
 }
 
-
 lapply(methods, train_model)
 
 #-------------------------------------------------------------------------------
@@ -134,20 +127,25 @@ lapply(methods, train_model)
 #-------------------------------------------------------------------------------
 
 # Create model_list
-model_list <- list(baseline = model_glmnet, xgboost = model_xgbTree)
+model_list <- list(glmnet = model_glmnet, xgboost = model_xgbTree)
 
-# Pass model_list to resamples(): resamples
+# Pass model_list to resamples
 resamples <- resamples(model_list)
 
 # Summarize the results
 summary(resamples)
-
 bwplot(resamples, metric = "F1")
 
+# get ROC results
+#install.packages("MLeval")
+library(MLeval)
+evaluation <- evalm(model_list,gnames=c('glmnet','xgboost'))
 
 #-------------------------------------------------------------------------------
-# MODEL PERFORMANCE - CONFUSION MATRIX
+# MODEL PERFORMANCE - CONFUSION MATRIX & VISuALIZATION
 #-------------------------------------------------------------------------------
+# visualize tuning parameters against performance
+plot(model_xgbTree, type=c("g", "o"))
 
 # cross-validated training performance
 # assess winner model on test data
@@ -159,14 +157,12 @@ xgb_pred_test <- predict.train(model_xgbTree, test, type = "raw")
 bal_accuracy(test, truth = test$Attrition, estimate = xgb_pred_test)
 confusionMatrix(xgb_pred_test, test$Attrition, mode = "prec_recall")
 
-
 #-------------------------------------------------------------------------------
 # PREDICTION ON ACTIVE EMPLOYEES
 #-------------------------------------------------------------------------------
 
-
 # save employees who did not churn
-no_churn <- subset(ibm_147,Attrition == "No")
+no_churn <- subset(test,Attrition == "No")
 # get class probabilities from employees who are still active
 probs <- predict.train(model_xgbTree, no_churn, type = "prob")
 # merge with employee data
@@ -177,3 +173,13 @@ still_active <- data.table(
 # show employees who are most at risk to leave soon
 head(still_active[order(-Yes)],5)
 
+#-------------------------------------------------------------------------------
+# BONUS: Feature importance
+#-------------------------------------------------------------------------------
+
+# estimate variable importance
+importance <- varImp(model_xgbTree, scale=FALSE)
+# summarize importance
+importance
+# plot importance
+plot(importance)
